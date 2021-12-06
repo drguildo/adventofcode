1e7a4f561a6500c026f7b30e8832675df8666b87
Simon Morgan <sjm@sjm.io>
2021-12-06 18:36:32 +0000

    Add Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
 use std::io::Read;
 
-fn day1_part1(input: Vec<&str>) -> u16 {
-    let input_nums: Vec<i32> = input.iter().map(|l| l.parse::<i32>().unwrap()).collect();
-    let mut input_iter = input_nums.iter();
+fn count_increases(nums: &Vec<i32>) -> u16 {
+    let mut input_iter = nums.iter();
 
     let mut increases = 0;
     let mut previous = input_iter.next().unwrap();
-    for i in &input_nums {
+    for i in nums {
         if i > previous {
             increases += 1;
         }
         previous = i;
     }
     increases
+}
+
+fn day1_part1(input: &Vec<&str>) -> u16 {
+    let input_nums: Vec<i32> = input.iter().map(|l| l.parse::<i32>().unwrap()).collect();
+    count_increases(&input_nums)
+}
+
+fn day1_part2(input: &Vec<&str>) -> u16 {
+    let input_nums: Vec<i32> = input.iter().map(|l| l.parse::<i32>().unwrap()).collect();
+
+    let mut sliding_windows: Vec<Vec<i32>> = Vec::new();
+    let mut i: usize = 0;
+    while i < input_nums.len() - 2 {
+        let mut sliding_window: Vec<i32> = Vec::new();
+        let mut num_taken = 0;
+        while num_taken < 3 {
+            sliding_window.push(input_nums[i + num_taken]);
+            num_taken += 1;
+        }
+        sliding_windows.push(sliding_window);
+        i += 1;
+    }
+
+    let sum_of_sliding_windows: Vec<i32> = sliding_windows.iter().map(|w| w.iter().sum()).collect();
+    count_increases(&sum_of_sliding_windows)
 }
 
 fn main() {
@@ -29,5 +53,6 @@ fn main() {
         .expect("Failed to convert input file to string");
     let input: Vec<&str> = input_string.lines().collect();
 
-    println!("{}", day1_part1(input));
+    println!("Day 1, Part 1: {}", day1_part1(&input));
+    println!("Day 1, Part 2: {}", day1_part2(&input));
 }
