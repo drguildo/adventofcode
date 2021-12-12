54a3c28cd8c6070bfee18878d7f62b6d912fbe86
Simon Morgan <sjm@sjm.io>
2021-12-12 18:37:52 +0000

    Get rid of repeated division

--- src/day3.rs
+++ src/day3.rs
@@ -9,12 +9,12 @@ pub fn day3_part1(input: &[String]) -> u32 {
         }
     }
 
-    let input_len = input.len() as u16;
+    let half_len = (input.len() / 2) as u16;
 
     let mut gamma: u32 = 0;
     for count in counter {
         gamma = gamma << 1;
-        if count > (input_len / 2) {
+        if count > half_len {
             gamma += 1;
         }
     }
